00e04820f307cd694538b00b8ed2d726813c3483
laurenbenichou <laurenbenichou@gmail.com>
2015-07-28 17:24:21 -0700

    rough draft of build-out done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$("document").ready(function(){
+  var waypoint = new Waypoint({
+    element: $('#play'),
+    handler: function() {
+      $('.media-title').addClass("load");
+      setTimeout( function(){$('.media-title-sec').addClass("load")}, 1000);
+    },
+  });
 
+function init(){
+  $("img.lazy").lazyload({
+    effect : "fadeIn"
+  });
+  $("#main-el").addClass("load");
+}
+
+  init();
+});
