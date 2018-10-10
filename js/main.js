9e08d1b03a8825edb742f02cf9383bfd3d0520ab
Radames Ajna <radamajna@gmail.com>
2018-10-10 15:48:59 -0700

    new nav brand

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-$("document").ready(function(){
-  var waypoint = new Waypoint({
-    element: $('#play'),
-    handler: function() {
-      $('.media-title-first').addClass("load");
-      setTimeout( function(){$('.media-title-sec').addClass("load")}, 2500);
-    },
-  });
+$("document").ready(function() {
+    var waypoint = new Waypoint({
+        element: $('#play'),
+        handler: function() {
+            $('.media-title-first').addClass("load");
+            setTimeout(function() {
+                $('.media-title-sec').addClass("load")
+            }, 2500);
+        },
+    });
 
-function init(){
-  $("img.lazy").lazyload({
-    effect : "fadeIn"
-  });
-  $("#main-el").addClass("load");
-}
+    function init() {
+        $("img.lazy").lazyload({
+            effect: "fadeIn"
+        });
+        $("#main-el").addClass("load");
+        audiojs.events.ready(function() {
+            var as = audiojs.createAll();
+        });
+    }
 
-  init();
+    init();
 });
